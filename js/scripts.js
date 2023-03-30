98cba71e55cdc7304deec3fb912ea3447d7db467
Tobias Llop <tobiasllop@gmail.com>
2023-03-30 00:54:34 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,28 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+// Función para detectar cuando el elemento está en la pantalla
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Función para agregar la clase "fade-in" cuando el elemento está en la pantalla
+function fadeInOnScroll() {
+  var elements = document.querySelectorAll('.fade-in-text');
+  for (var i = 0; i < elements.length; i++) {
+    if (isElementInViewport(elements[i])) {
+      elements[i].classList.add('fade-in');
+    }
+  }
+}
+
+// Event listener para detectar el scroll
+window.addEventListener('scroll', function() {
+  fadeInOnScroll();
+});
